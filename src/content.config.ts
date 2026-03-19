5447790438a345559419f4595266a50376e8f196
Manuel <manuelalberto.gorrin@gmail.com>
2026-03-18 22:14:14 -0400

    feat: add loader to work collection for improved content management

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,11 @@
 import { z } from 'astro/zod';
 import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+
 
 export const collections = {
 	work: defineCollection({
+		loader: glob({ base: './src/content/work', pattern: '**/*.{md,mdx}' }),
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
